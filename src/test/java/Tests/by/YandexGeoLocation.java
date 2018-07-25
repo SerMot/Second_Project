cee5e855bc2735d4b3766799a91065058c5aa76c
SerMot <41292432+SerMot@users.noreply.github.com>
2018-07-25 09:54:00 +0300

    Update YandexGeoLocation.java
    Сокращено до одного теста, добавлено сравнение

--- src/test/java/Tests/by/YandexGeoLocation.java
+++ src/test/java/Tests/by/YandexGeoLocation.java
@@ -1,52 +1,41 @@
 package Tests.by;
 
 import Driver.CreateCloseDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
+import java.util.ArrayList;
 
 public class YandexGeoLocation extends CreateCloseDriver {
     @Test
-    public void londonTest() {
-        String URL = "https://yandex.by";
-        mainPage.navigate(URL);
+    public void cityTest() {
 
-        mainPage.clickGeoLink();
+        String urlLondon = "https://yandex.by";
+        mainPage.navigateLondon(urlLondon);
+        mainPage.clickGeoLink("Лондон");
         mainPage.inputGeoField("Лондон");
         mainPage.clickLodonList();
-        mainPage.clickMoreLink();
-    }
+        mainPage.clickMoreLink("Лондон");
 
-    @Test
-    public void parisTest() {
-        String URL = "https://yandex.by";
-        mainPage.navigate(URL);
-        mainPage.clickGeoLink();
+        String urlParis = "https://yandex.by";
+        mainPage.navigateParis(urlParis);
+        mainPage.clickGeoLink("Париж");
         mainPage.inputGeoField("Париж");
-        mainPage.clickFranceList();
-        mainPage.clickMoreLink();
-    }
+        mainPage.clickParisList();
+        mainPage.clickMoreLink("Париж");
 
-    @Test
-    public void compareMoreList() {
-        String URL = "https://yandex.by";
-        mainPage.navigate(URL);
+        ArrayList<String> moreLinkCityLondon;
+        ArrayList<String> moreLinkCityParis;
 
-        List<WebElement> londonLists = driver.findElements(By.cssSelector("div[class=\"home-tabs__more-item\"] a"));
-        List<WebElement> parisLists = driver.findElements(By.cssSelector("div[class=\"home-tabs__more-item\"] a"));
+        mainPage.clickMoreLink("Лондон");
+        moreLinkCityLondon = mainPage.getCountMoreLinks("Лондон");
 
-        Assert.assertEquals(londonLists, parisLists);
-        for (int i = 0; i < londonLists.size(); i++) {
-        Assert.assertEquals(londonLists.get(i).getText(), parisLists.get(i).getText());
+        mainPage.clickMoreLink("Париж");
+        moreLinkCityParis = mainPage.getCountMoreLinks("Париж");
+        for (int i = 0; i < moreLinkCityLondon.size(); i++) {
+            Assert.assertEquals(moreLinkCityLondon.get(i), moreLinkCityParis.get(i));
 
         }
     }
 }
-
-
-
-
